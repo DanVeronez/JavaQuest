f2cdbebc4960e79c098fb749a5bf8d9651fd3e6b
Daniel Veronez <danielveronezb@gmail.com>
2024-11-13 09:24:42 -0300

    primeiro commit

---
+++ src/test/java/com/javaquest/javaquest/JavaquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaquest.javaquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
